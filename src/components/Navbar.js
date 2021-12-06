23270050ee628ffb29e6e00ad9b1226e1121feb9
arybyt <bayatiarya@gmail.com>
2021-12-05 22:25:04 -0600

    innit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react';
 import { Button } from './Button';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
-import logo from '../assets/images/jefflogo.png';
 function Navbar() {
   const [click, setClick] = useState(false);
   const [button, setButton] = useState(true);
